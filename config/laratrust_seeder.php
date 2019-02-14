799b47c17b92ac09431666a185281523aaf99804
Ha Linh <havanlinh1996@gmail.com>
2019-02-14 16:24:56 +0700

    Cập nhật seeder

--- config/laratrust_seeder.php
+++ config/laratrust_seeder.php
@@ -1,42 +1,42 @@
 <?php
 
 return [
-    'role_structure' => [
+    'role_structure'       => [
 
-        'superadministrator' => [
-            'users' => 'c,r,u,d',
-            'acl' => 'c,r,u,d',
+        'administrator' => [
+            'users'   => 'c,r,u,d',
+            'acl'     => 'c,r,u,d',
             'profile' => 'r,u'
         ],
 
-'buyer' => [
-    'users' => 'c,r,u,d',
-    'ou' => 'c,r,u,d',
-    'acl' => 'c,r,u,d',
+        'buyer' => [
+            'users' => 'c,r,u,d',
+            'ou'    => 'c,r,u,d',
+            'acl'   => 'c,r,u,d',
 
-],
+        ],
 
-'supplier' => [
-    'users' => 'c,r,u,d',
-    'ou' => 'c,r,u,d',
-    'acl' => 'c,r,u,d',
+        'supplier' => [
+            'users' => 'c,r,u,d',
+            'ou'    => 'c,r,u,d',
+            'acl'   => 'c,r,u,d',
 
-],
+        ],
 
-'manufacture' => [
-    'profile' => 'r,u'
-],
+        'manufacture' => [
+            'profile' => 'r,u'
+        ],
 
-'hr' => [
-    'profile' => 'r,u'
-],
+        'human resource' => [
+            'profile' => 'r,u'
+        ],
     ],
     'permission_structure' => [
         'cru_user' => [
             'profile' => 'c,r,u'
         ],
     ],
-    'permissions_map' => [
+    'permissions_map'      => [
         'c' => 'create',
         'r' => 'read',
         'u' => 'update',
