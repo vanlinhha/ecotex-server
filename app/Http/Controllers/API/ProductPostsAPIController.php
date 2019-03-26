661661848db95197bb632ed5d1be2bee2ff9f664
Ha Linh <havanlinh1996@gmail.com>
2019-03-26 11:29:28 +0700

    Cập nhật ProductPosts API

--- app/Http/Controllers/API/ProductPostsAPIController.php
+++ app/Http/Controllers/API/ProductPostsAPIController.php
@@ -105,7 +105,7 @@ class ProductPostsAPIController extends AppBaseController
             $productPost['attached_files'] = $attachedFiles;
             $attachedImages = $productPost->attachedImages()->get();
             $productPost['images'] = $attachedImages;
-            $productPost['creator'] = $productPost->creator()->get(['first_name', 'last_name']);
+            $productPost['creator'] = $productPost->creator()->get(['first_name', 'last_name', 'company_name']);
         }
 
         return $this->sendResponse($productPosts->toArray(), 'Product Posts retrieved successfully');
@@ -196,7 +196,7 @@ class ProductPostsAPIController extends AppBaseController
         $productPosts['attached_files'] = $attachedFiles;
         $attachedImages = $productPosts->attachedImages()->get();
         $productPosts['images'] = $attachedImages;
-        $productPosts['creator'] = $productPosts->creator()->get(['first_name', 'last_name']);
+        $productPosts['creator'] = $productPosts->creator()->get(['first_name', 'last_name', 'company_name']);
 
 
         return $this->sendResponse($productPosts->toArray(), 'Product Posts saved successfully');
@@ -421,7 +421,7 @@ class ProductPostsAPIController extends AppBaseController
             $productPost['attached_files'] = $attachedFiles;
             $attachedImages = $productPost->attachedImages()->get();
             $productPost['images'] = $attachedImages;
-            $productPost['creator'] = $productPost->creator()->get(['first_name', 'last_name']);
+            $productPost['creator'] = $productPost->creator()->get(['first_name', 'last_name', 'company_name']);
         }
 
         return $this->sendResponse($productPosts->toArray(), 'Product Posts retrieved successfully');
