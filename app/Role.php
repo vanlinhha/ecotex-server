41dda6a3f2a00253de73e9431e14a9ce7a23c26c
Ha Linh <havanlinh1996@gmail.com>
2019-04-04 16:34:47 +0700

    Sửa lỗi

--- app/Role.php
+++ app/Role.php
@@ -7,6 +7,6 @@ use Laratrust\Models\LaratrustRole;
 class Role extends LaratrustRole
 {
     //
-    protected $hidden = ['created_at', 'updated_at', 'pivot'];
+    protected $hidden = ['created_at', 'updated_at'];
 
 }
