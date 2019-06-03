1dca1afd7c33d00770d21dd9944827e246bef95c
Ha Van Linh <havanlinh1996@gmail.com>
2019-06-03 10:15:03 +0700

    Cập nhật ProductPostsAPIController

--- app/Http/Controllers/API/ProductPostsAPIController.php
+++ app/Http/Controllers/API/ProductPostsAPIController.php
@@ -323,8 +323,54 @@ class ProductPostsAPIController extends AppBaseController
         if (empty($productPosts)) {
             return $this->sendError(__('Product Posts not found'));
         }
+        DB::beginTransaction();
+        try{
+            $productPosts = $this->productPostsRepository->update($input, $id);
 
-        $productPosts = $this->productPostsRepository->update($input, $id);
+            // xóa file cũ
+            foreach ($productPosts->attachedFiles()->get() as $item) {
+                unlink(substr($item['url'], 1));
+            }
+            foreach ($productPosts->attachedImages() as $item) {
+                unlink(substr($item['url'], 1));
+            }
+
+            $productPosts->attachedFiles()->delete();
+            $productPosts->attachedImages()->delete();
+
+            if ($request->hasFile('images')) {
+                foreach ($request->file('images') as $image) {
+                    $extension = $image->getClientOriginalName();
+                    $filename  = uniqid() . '-' . $extension;
+                    $image->move(storage_path('app/public/images'), $filename);
+
+                    $productPosts->attachedImages()->create(['url' => '/storage/images/' . $filename, 'name' => $extension]);
+                }
+            }
+
+            if ($request->hasFile('attached_files')) {
+                foreach ($request->file('attached_files') as $file) {
+
+                    $extension = $file->getClientOriginalName();
+                    $filename  = uniqid() . '-' . $extension;
+                    $file->move(storage_path('app/public/files'), $filename);
+
+                    $productPosts->attachedFiles()->create(['url' => '/storage/files/' . $filename, 'name' => $extension]);
+                }
+            }
+            DB::commit();
+        }
+        catch (\Exception $exception){
+            DB::rollBack();
+            $this->sendError(__("Can not create posts, please try again later!"), 500);
+        }
+
+
+        $attachedFiles                  = $productPosts->attachedFiles()->get();
+        $productPosts['attached_files'] = $attachedFiles;
+        $attachedImages                 = $productPosts->attachedImages()->get();
+        $productPosts['images']         = $attachedImages;
+        $productPosts['creator']        = $productPosts->creator()->select('first_name', 'last_name', 'company_name', 'avatar')->first();
 
         return $this->sendResponse($productPosts->toArray(), 'ProductPosts updated successfully');
     }
