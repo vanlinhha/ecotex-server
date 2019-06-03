d7dcc41f50895d0bc402a98dc796899b156811ec
Ha Van Linh <havanlinh1996@gmail.com>
2019-06-03 14:25:27 +0700

    Cap nhat bao cao

--- app/Http/Controllers/API/ReportAPIController.php
+++ app/Http/Controllers/API/ReportAPIController.php
@@ -66,7 +66,7 @@ class ReportAPIController extends AppBaseController
         $arr['total_products'] = $this->productsRepository->all()->count();
         $arr['total_inactivated_users'] = $this->usersRepository->findWhere([['is_activated', '=', 0]])->count();
         $arr['total_activated_users'] = $this->usersRepository->findWhere([['is_activated', '=', 1]])->count();
-        $arr['total_users'] = $this->usersRepository->all()->count();
+//        $arr['total_users'] = $this->usersRepository->all()->count();
         $arr['total_conversations'] = Conversation::all()->count();
         $arr['total_messages'] = Message::all()->count();
         $arr['total_location'] = Locations::all()->count();
@@ -84,13 +84,33 @@ class ReportAPIController extends AppBaseController
         $date = \DateTime::createFromFormat('Y-m-d',$start_date);
         $date->modify('-30 day');
 
-        $arr['total_users_last_30_days'] = $this->usersRepository->findWhere([['created_at', '>=', $date]])->count();
+//        $arr['total_users_last_30_days'] = $this->usersRepository->findWhere([['created_at', '>=', $date]])->count();
         $arr['total_inactivated_users_last_30_days'] = $this->usersRepository->findWhere([['created_at', '>=', $date],['is_activated', '=', 0]])->count();
         $arr['total_activated_users_last_30_days'] = $this->usersRepository->findWhere([['created_at', '>=', $date],['is_activated', '=', 1]])->count();
         $arr['total_conversations_last_30_days'] = Conversation::where('created_at', '>=', $date)->count();
         $arr['total_messages_last_30_days'] = Message::where('created_at', '>=', $date)->count();
         $arr['total_product_posts_last_30_days'] = $this->productPostsRepository->findWhere([['created_at', '>=', $date]])->count();
 
+        for ($i = 1; $i <= 6; $i++)
+        {
+            $temp = [];
+            $temp['month'] = date("m-Y", strtotime( date( 'Y-m-01' )." -$i months"));
+            $first_day = date('Y-m-01', strtotime($temp['month']));
+            $last_day = date('Y-m-t', strtotime($temp['month']));
+
+            $temp['total_products'] = $this->productsRepository->findWhere([['created_at', '>=', $first_day],['created_at', '<=', $last_day]])->count();
+            $temp['total_inactivated_users'] = $this->usersRepository->findWhere([['is_activated', '=', 0],['created_at', '>=', $first_day],['created_at', '<=', $last_day]])->count();
+            $temp['total_activated_users'] = $this->usersRepository->findWhere([['is_activated', '=', 1],['created_at', '>=', $first_day],['created_at', '<=', $last_day]])->count();
+            $temp['total_conversations'] = Conversation::where([['created_at', '>=', $first_day],['created_at', '<=', $last_day]])->count();
+            $temp['total_location'] = Locations::where([['created_at', '>=', $first_day],['created_at', '<=', $last_day]])->count();
+            $temp['total_brands'] = Brand::where([['created_at', '>=', $first_day],['created_at', '<=', $last_day]])->count();
+            $temp['total_responses'] = Responses::where([['created_at', '>=', $first_day],['created_at', '<=', $last_day]])->count();
+            $temp['total_product_posts'] = $this->productPostsRepository->findWhere([['created_at', '>=', $first_day],['created_at', '<=', $last_day]])->count();
+            $temp['total_job_posts'] = $this->jobPostsRepository->findWhere([['created_at', '>=', $first_day],['created_at', '<=', $last_day]])->count();
+
+            $months[] = $temp;
+        }
+        $arr['last_6_month'] = $months;
 
         return $arr;
 
